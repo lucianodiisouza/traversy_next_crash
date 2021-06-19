022404836828dc5f31af4b0ab51b76f7b490d5ac
Luciano dii Souza <luciano.souza@rarolabs.com.br>
2021-06-19 01:35:02 -0300

    updating...

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://www.meusite.com";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://traversy-next-crash.vercel.app";
